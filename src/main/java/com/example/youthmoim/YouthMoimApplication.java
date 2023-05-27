6873add801d598618681c55fde9a0f41be286e9a
sang5c <skah321@gmail.com>
2023-05-27 17:36:28 +0900

    init

---
+++ src/main/java/com/example/youthmoim/YouthMoimApplication.java
@@ -0,0 +1,13 @@
+package com.example.youthmoim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthMoimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouthMoimApplication.class, args);
+    }
+
+}
